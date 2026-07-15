15b949eea1d6a718d316f340e908f4abd5cd8606
EasinTanvir <easintanvir102@gmail.com>
2026-07-15 19:53:32 +0600

    remove clerk

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,14 +1,5 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
 export default async function DashboardRouteLayout({ children }) {
-  await auth.protect();
-  const { sessionClaims } = await auth();
-  if (sessionClaims?.metadata?.role !== "admin") {
-    redirect("/");
-  }
-
-  console.log("sessionClaims", sessionClaims);
   return <DashboardLayout>{children}</DashboardLayout>;
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,20 +25,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full bg-slate-100 text-slate-950">
-        <ClerkProvider>
-          <Show when="signed-out">
-            <SignInButton />
-            <SignUpButton>
-              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </Show>
-          <Show when="signed-in">
-            <UserButton />
-          </Show>
-          <main>{children}</main>
-        </ClerkProvider>
+        <main>{children}</main>
       </body>
     </html>
   );
