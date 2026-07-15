95c9e6438f3f876328c239c17687c3f8587b8e6d
EasinTanvir <easintanvir102@gmail.com>
2026-07-15 15:15:48 +0600

    remove clerk

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,5 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
-import { auth } from "@clerk/nextjs/server";
+
 export default async function DashboardRouteLayout({ children }) {
-  await auth.protect();
   return <DashboardLayout>{children}</DashboardLayout>;
 }
