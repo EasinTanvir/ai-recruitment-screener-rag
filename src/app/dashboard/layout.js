62e03ba6d1dced041c9d18fa023ea775cdf70439
EasinTanvir <easintanvir102@gmail.com>
2026-07-15 15:20:45 +0600

    added clerk authentication

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,5 +1,7 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function DashboardRouteLayout({ children }) {
+  await auth.protect();
   return <DashboardLayout>{children}</DashboardLayout>;
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
-
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,7 +30,22 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full bg-slate-100 text-slate-950">{children}</body>
+      <body className="min-h-full bg-slate-100 text-slate-950">
+        <ClerkProvider>
+          <Show when="signed-out">
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+          <main>{children}</main>
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
