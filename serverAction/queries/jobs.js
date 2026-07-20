a8b6dec53da7eaf80514d3b1d4b4560931c32235
EasinTanvir <easintanvir102@gmail.com>
2026-07-21 00:59:03 +0600

    added job details

--- serverAction/queries/jobs.js
+++ serverAction/queries/jobs.js
@@ -1,16 +1,47 @@
 import { db } from "@/lib/db";
-import { jobs } from "@/lib/schema";
+import {
+  aiEvaluations,
+  applicantMetadata,
+  applications,
+  jobs,
+} from "@/lib/schema";
 import { desc, eq } from "drizzle-orm";
 
 export async function getJobs() {
   return await db.select().from(jobs).orderBy(desc(jobs.createdAt));
 }
-
 export async function getJobById(id) {
-  const [job] = await db
-    .select()
-    .from(jobs)
-    .where(eq(jobs.id, Number(id)));
+  const jobId = Number(id);
 
-  return job;
+  const [job] = await db.select().from(jobs).where(eq(jobs.id, jobId));
+
+  if (!job) {
+    return null;
+  }
+
+  const applicants = await db
+    .select({
+      id: applications.id,
+      status: applications.status,
+      createdAt: applications.createdAt,
+
+      firstName: applicantMetadata.firstName,
+      lastName: applicantMetadata.lastName,
+      email: applicantMetadata.email,
+
+      overallScore: aiEvaluations.overallScore,
+    })
+    .from(applications)
+    .leftJoin(
+      applicantMetadata,
+      eq(applications.id, applicantMetadata.applicationId),
+    )
+    .leftJoin(aiEvaluations, eq(applications.id, aiEvaluations.applicationId))
+    .where(eq(applications.jobId, jobId))
+    .orderBy(desc(aiEvaluations.overallScore));
+
+  return {
+    ...job,
+    applicants,
+  };
 }
